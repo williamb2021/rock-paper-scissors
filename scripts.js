2d4b31eba3560f54ec92c6f4f894547df19eb2f2
WilliamB2021 <bonewj@gmail.com>
2021-12-23 18:50:48 -0800

    Functioning game by typing game() into console.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,61 @@
-console.log("Hello");
+function computerSelection(){
+    let random = Math.random();
+    let choice;
+    if (random < 0.33){
+        choice = "rock";
+    }
+    else if (random >= 0.33 && random < 0.66){
+        choice = "paper";
+    }
+    else {
+        choice = "scissors";
+    }
+   return choice; 
+}
+
+function playerSelection(){
+    return prompt("Rock, paper, or scissors?","Rock").toLowerCase();
+}
+
+
+function playRound(playerSelection, computerSelection){
+    let result;
+    if ((playerSelection === "rock" && computerSelection === "scissors")
+        || (playerSelection === "paper" && computerSelection === "rock")
+        || (playerSelection === "scissors" && computerSelection == "paper")){
+        result = "player";
+    }
+    else if (playerSelection===computerSelection){
+        result = "tie";
+    }
+    else{
+        result = "computer";
+    }
+    return result;
+}
+
+function game(){
+    let computerScore = 0;
+    let playerScore = 0;
+    for ( let i=0 ; i<5 ; i++ ) {
+        let game = playRound(playerSelection(), computerSelection());
+        if (game === "player"){
+            playerScore +=1;
+        }
+        else if (game === "computer"){
+            computerScore +=1;
+        }
+        console.log("Computer score: " + computerScore + "\n"
+                    + "Player score: " + playerScore )        
+    }
+    if (playerScore > computerScore){
+        console.log("Player wins!");
+    }
+    else if (computerScore > playerScore){
+        console.log("Computer wins!");
+    }
+    else {
+        "It's a tie! That's no fun, so try again :)"
+    }
+    
+}
